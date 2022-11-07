82aa04040b57ca9e6dc180ca34539c934834a78b
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-06 23:04:50 -0600

    ✨ Add feed section and styles

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Delivery from './Components/Body/Delivery';
 import Login from './Components/Body/Login';
+import Feed from './Pages/Feed/Feed';
 import { Routes, Route} from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />}/>
         <Route path='/delivery' element={<Delivery />}/> 
+        <Route path='/feed' element={<Feed />}/> 
       </Routes>
       <Footer />
     </div>

e78ae1dfc72ea8aa3320871bfd97dd070939ad80
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-07 10:46:04 -0600

    ✨ Add ProductCard component and DecorativeLines component

--- Client/src/Pages/Feed/Feed.jsx
+++ Client/src/Pages/Feed/Feed.jsx
@@ -1,93 +1,24 @@
 import React from 'react'
 import './Feed.scss';
+import DecorativeLines from './../../Components/Body/DecorativeLines/DecorativeLines'
+import ProductCard from './../../Components/Body/ProductCard/ProductCard'
 
 const Feed = () => {
   return (
     <section className="recommended-products">
-        <div className="upper-lines">
-            <hr className="line-1"/>
-            <hr className="line-2"/>
-            <hr className="line-3"/>
-            <hr className="line-4"/>
-        </div>
-        <div className="lower-lines">
-            <hr className="line-1"/>
-            <hr className="line-2"/>
-            <hr className="line-3"/>
-            <hr className="line-4"/>
-        </div>
+        <DecorativeLines />
         <h1 className="title">Recomendados para ti</h1>
         <div className="cards">
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
-            <article className="product-card">
-                <figure>
-                    <img src="./assets/img/product.jpg" alt=""/>
-                </figure>
-                <p className="product-name">Cartera Christian Dior</p>
-                <p className="product-price">$30.00</p>
-            </article>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </div>
     </section>
   );

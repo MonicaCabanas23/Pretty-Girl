3a2dadbb53d3b7d390888ff57a3e7adbadb0f737
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-07 23:15:17 -0600

    ✨ Add ProductsCards and CategoriesCards hover

--- Client/src/Components/Body/CategoriesContainer/CategoryCard/CategoryCard.jsx
+++ Client/src/Components/Body/CategoriesContainer/CategoryCard/CategoryCard.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './CategoryCard.scss'
 
 const CategoryCard = () => {
+  /* const [categories, setCategories] = useState([]); */
+
   return (
     <article className="category-card">
         <figure>
             <img src="./img/accesorios" alt=""/>
+            
+            <div className="hover-container">
+              <div className="hover">
+              <p>Text</p>
+              </div>
+          </div>
         </figure>
-        {/* <div className="hover">
-            <p>Text</p>
-        </div> */}
     </article>
   )
 }

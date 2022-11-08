e62b582506a20caf657348413a80ca37292f2daa
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-07 21:46:08 -0600

    ✨ Add footer information components

---
+++ Client/src/Components/Footer/ContactInformation/ContactInformation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ContactInformation.scss'
+
+const ContactInformation = () => {
+  return (
+    <div className="footer-contact-info">
+        <h3>Información de contacto</h3>
+        <p>Dirección</p>
+        <p>Correo electrónico</p>
+        <p>+503 7434 3732</p>
+    </div>
+  )
+}
+
+export default ContactInformation

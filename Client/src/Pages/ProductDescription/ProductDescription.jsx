1f0d411ac4bf84e9100ff947e8e2b621b2d15b2a
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-08 11:51:48 -0600

    ✨ Add Header to login component

---
+++ Client/src/Pages/ProductDescription/ProductDescription.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProductDescription.scss'
+
+const ProductDescription = () => {
+  return (
+    <div>ProductDescription</div>
+  )
+}
+
+export default ProductDescription

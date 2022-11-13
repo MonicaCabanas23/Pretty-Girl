0bae73de874df82413ee059ab87ab6f7818eba2e
Marroquin <54285979+Marroquin02@users.noreply.github.com>
2022-11-13 11:18:06 -0600

    :sparkles: Update: Login y Form

--- Client/src/Components/Form/Form.jsx
+++ Client/src/Components/Form/Form.jsx
@@ -1,31 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import './Form.scss'
 import Label from '../Label/Label'
 import Description from './Description/Description';
 import Button from '../Button/Button'
+import A from '../a/a'
 
 /* A form can have different types in this app: login, register, client-data, delivery-info, description*/
 /* cancelHandle y continueHandle son parámetros para funciones en caso de que se de click en esos bootones */
-const Form = ({title, formType, formFields, descriptionFields, justContinue, cancelHandle, cancelPath, cancelText, continueHandle, continuePath, continueText}) => {
+const Form = ({ title, formType, formFields, descriptionFields, justContinue, cancelHandle, cancelPath, cancelText, continueHandle, continuePath, continueText }) => {
     const [fields, setFields] = useState([]);
 
     /* When render just once */
     useEffect(() => {
         if (formType != 'description') {
-            const mappedForm = formFields.map( field => {
-                return(
-                    <Label  key={field.key} type={field.type} text={field.text} valueInput={field.value} setValue={field.setValue}/>
-                )
+            const mappedForm = formFields.map(field => {
+                if (field.element === 'label') {
+                    return (
+                        <Label key={field.key} type={field.type} text={field.text} valueInput={field.value} setValue={field.setValue} />
+                    )
+                }
+                else if (field.element === 'a') {
+                    return (
+                        <A key={field.key} href={field.href} text={field.text} />
+                    )
+                }
             });
             setFields(mappedForm);
         }
 
         /* Si no hay campos que el usuario deba de llenar entonces utilizaremos el form del tipo description */
-        if (formType === 'description'){
-            const mappedDescription = descriptionFields.map( field => {
-                return(
-                    <Description key={field.key} title={field.title} descriptionObject={field.object}/>
+        if (formType === 'description') {
+            const mappedDescription = descriptionFields.map(field => {
+                return (
+                    <Description key={field.key} title={field.title} descriptionObject={field.object} />
                 )
             });
             setFields(mappedDescription);
@@ -33,22 +41,22 @@ const Form = ({title, formType, formFields, descriptionFields, justContinue, can
 
     }, []);
 
-  return (
-    <div className='form-container'>
-        <form className={formType}>
-            <h1>{title}</h1>
-            <div className="formFields">
-                {fields}
-            </div>
-            <div className="actions">
-                {
-                    justContinue ? <></> : <Link to={cancelPath}><Button clase='cancel' onClick={cancelHandle} text={cancelText}/></Link> 
-                }
-                <Link to={continuePath}><Button clase='continue' onClick={continueHandle} text={continueText}/></Link>
-            </div>
-        </form>
-    </div>
-  )
+    return (
+        <div className='form-container'>
+            <form className={formType}>
+                <h1>{title}</h1>
+                <div className="formFields">
+                    {fields}
+                </div>
+                <div className="actions">
+                    {
+                        justContinue ? <></> : <Link to={cancelPath}><Button clase='cancel' onClick={cancelHandle} text={cancelText} /></Link>
+                    }
+                    <Link to={continuePath}><Button clase='continue' onClick={continueHandle} text={continueText} /></Link>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Form

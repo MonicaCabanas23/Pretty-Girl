a7119bedf6eee26bf0c3dd00c1582ef3abbf963f
Marroquin <54285979+Marroquin02@users.noreply.github.com>
2022-11-21 19:08:29 -0600

    :sparkles: Add: Agregar a la bolsa y alertas

--- Client/src/Components/MainFeed/ProductDescription/ProductDescription.jsx
+++ Client/src/Components/MainFeed/ProductDescription/ProductDescription.jsx
@@ -5,6 +5,9 @@ import Loading from '../../Loading/Loading';
 import ProductsContainer from '../ProductsContainer/ProductsContainer';
 import { useConfigContext } from '../../../Contexts/ConfigContext';
 const ContainerPD = React.lazy(() => import('./ContainerPD/ContainerPD'));
+import Swal from 'sweetalert2'
+
+const [agregando, setAgregando] = useState(false);
 
 function ProductDescription({ id }) {
 
@@ -13,6 +16,7 @@ function ProductDescription({ id }) {
   const [encontrado, setEncontrado] = useState(false);
   const [loading, setLoading] = useState(true);
   const context = useConfigContext();
+
   useEffect(() => {
 
     setLoading(true)
@@ -32,8 +36,7 @@ function ProductDescription({ id }) {
       const color = (data.color).map(item => {
         return { value: item }
       });
-
-
+      console.log(context.isLogged)
       const formFields = [{
         'key': '1',
         'element': 'label',
@@ -80,17 +83,16 @@ function ProductDescription({ id }) {
         'key': '7',
         'element': context.isLogged ? 'button' : '',
         'text': 'Agregar a la bolsa',
-        'onClick': () => { console.log('Agregar al la bolsa') },
+        'onClick': () => { PushBag(id) },
         'clase': 'AgregarCarrito'
       },
       ]
       setFields(formFields);
       setEncontrado(true);
       setLoading(false)
-        
+
     }
     getData();
-      console.log(loading)
 
   }, [id])
 
@@ -109,3 +111,92 @@ function ProductDescription({ id }) {
   );
 }
 export default ProductDescription;
+
+
+async function PushBag(id) {
+  if (!agregando) {
+    setAgregando(true);
+    let url = "/api/bags/637030075a61d40805581882";
+    const config = {
+      headers: {
+        'x-token': localStorage.getItem("token")
+      }
+    };
+    await axios.get(url, config).then(datos => {
+      if (datos.data.length == 0) {
+        url = "/api/bags/";
+        const config = {
+          headers: {
+            'x-token': localStorage.getItem("token")
+          }
+        };
+        const data = {
+          user: '637030075a61d40805581882',
+          products: [
+            {
+              "_id": id,
+              amount: 1
+            }
+          ]
+        }
+        axios.post(url, data, config).then(data => {
+        }).catch(error =>
+          Swal.fire({
+            title: 'Ocurrio un error vuelve a intentarlo',
+            timer: 2000,
+            icon: 'error',
+            showConfirmButton: false,
+            timerProgressBar: true,
+            allowOutsideClick: false
+          })
+        )
+      }
+      else {
+        url = "/api/bags/" + datos.data[0]._id;
+        const config = {
+          headers: {
+            'x-token': localStorage.getItem("token")
+          }
+        };
+        let data = {
+          user: datos.data[0].user,
+          products: []
+        }
+        let update = false;
+        datos.data[0].products.forEach((element, index) => {
+          if (element._id == id) {
+            data.products.push({
+              "_id": element._id,
+              amount: element.amount + 1
+            })
+            update = true;
+          }
+        });
+        if (!update) data.products.push({
+          "_id": id,
+          amount: 1
+        })
+        axios.put(url, data, config).then(data => {
+          Swal.fire({
+            title: 'Agregado a la bolsa',
+            timer: 2000,
+            icon: 'success',
+            showConfirmButton: false,
+            timerProgressBar: true,
+            allowOutsideClick: false
+          })
+        }).catch(error =>
+          Swal.fire({
+            title: 'Ocurrio un error vuelve a intentarlo',
+            timer: 2000,
+            icon: 'error',
+            showConfirmButton: false,
+            timerProgressBar: true,
+            allowOutsideClick: false
+          })
+        )
+      }
+    })
+    setAgregando(false);
+  }
+}

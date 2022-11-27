7e5b267a6bdac3e2183c116e93acf02f29333ea8
Marroquin <54285979+Marroquin02@users.noreply.github.com>
2022-11-27 00:40:09 -0600

    :sparkles: Update: Bag mostrando talla y color

--- Client/src/Components/Booking/ProductCardBooking/ProductCardBooking.jsx
+++ Client/src/Components/Booking/ProductCardBooking/ProductCardBooking.jsx
@@ -9,6 +9,8 @@ const ProductCardBooking = ({ item, id = '', setTotal }) => {
         <img src={item.picture} alt={item.name} />
       </figure>
       <p className="product-name">{item.name}</p>
+      <p className="product-color">{item.color}</p>
+      <p className="product-talla">{item.size}</p>
       <p className="product-price">${item.price}</p>
       <p className="product-amount">Cantidad: {item.amount}</p>
       <p className="product-subtotal">SubTotal: ${precio}</p>

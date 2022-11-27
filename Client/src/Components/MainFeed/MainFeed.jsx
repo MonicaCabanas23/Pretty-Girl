ff24ac5ccaf6bd7590e2c308d3814c89ec9a5bf7
cabrera-evil <douglascabrera.dev@gmail.com>
2022-11-27 16:59:40 -0600

    Add: Filter by clicked category

--- Client/src/Components/MainFeed/MainFeed.jsx
+++ Client/src/Components/MainFeed/MainFeed.jsx
@@ -1,30 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './MainFeed.scss'
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import CategoriesContainer from './CategoriesContainer/CategoriesContainer'
 import ProductDescription from './ProductDescription/ProductDescription'
 import ProductsContainer from './ProductsContainer/ProductsContainer'
-import { useConfigContext} from '../../Contexts/ConfigContext'
+import { useNavigate } from 'react-router-dom';
+import { useConfigContext } from '../../Contexts/ConfigContext'
 
 const Body = () => {
   const context = useConfigContext();
   const [categoryClicked, setCategoryClicked] = useState('');
+
+  // Navigate to products filtered by selected category
+  const navigate = useNavigate();
+  const url = `/api/products?category=${categoryClicked}`;
 
   const handleCategoryClick = (value) => {
     setCategoryClicked(value);
   }
 
   useEffect(() => {
-    console.log(categoryClicked);
+    // Verify if categoryClicked is not empty
+    if(categoryClicked !== '') {
+      navigate('/feed/filtered', {
+        state: {
+          filteredUrl: url
+        }
+      });
+    }
   }, [categoryClicked])
-  
+
 
   return (
     <div className='main-feed-container'>
-      <CategoriesContainer handleCategoryClick={handleCategoryClick}/>
-      <ProductsContainer id={'arrived'} title={'Recién llegados'}/>
+      <CategoriesContainer handleCategoryClick={handleCategoryClick} />
+      <ProductsContainer id={'arrived'} title={'Recién llegados'} />
       {
-        context.isLogged ? <ProductsContainer id='recommended' title={'Recomendados para ti'}/> : <></>
+        context.isLogged ? <ProductsContainer id='recommended' title={'Recomendados para ti'} /> : <></>
       }
     </div>
   )

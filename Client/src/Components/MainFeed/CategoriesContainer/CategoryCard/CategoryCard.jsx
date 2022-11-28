7ab2e6cf87542b3072d8e2dceeca41ac7637b1af
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-27 18:02:50 -0600

    🐛 Delete smooth scroll for categories

--- Client/src/Components/MainFeed/CategoriesContainer/CategoryCard/CategoryCard.jsx
+++ Client/src/Components/MainFeed/CategoriesContainer/CategoryCard/CategoryCard.jsx
@@ -7,37 +7,16 @@ const CategoryCard = ({ image, name, onClickHandle }) => {
   const loggedContext = useConfigContext();
 
   return (
-    <>
-      {
-        loggedContext.isLogged  ?
-        <Link to='recommended' smooth={true} offset={-100} duration={1000}>
-          <article className="category-card" onClick={onClickHandle}>
-            <figure>
-              <img src={image} alt="" />
-
-              <div className="hover-container">
-                <div className="hover">
-                  <p>{name}</p>
-                </div>
-              </div>
-            </figure>
-          </article>
-        </Link> : 
-        <Link to='arrived' smooth={true} offset={-100} duration={1000}>
-          <article className="category-card" onClick={onClickHandle}>
-            <figure>
-              <img src={image} alt="" />
-
-              <div className="hover-container">
-                <div className="hover">
-                  <p>{name}</p>
-                </div>
-              </div>
-            </figure>
-          </article>
-        </Link>
-      }
-    </>
+    <article className="category-card" onClick={onClickHandle}>
+      <figure>
+        <img src={image} alt="" />
+        <div className="hover-container">
+          <div className="hover">
+            <p>{name}</p>
+          </div>
+        </div>
+      </figure>
+    </article>
   )
 }
 

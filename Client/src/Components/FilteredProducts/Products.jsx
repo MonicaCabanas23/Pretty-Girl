9fc7ca08af5b24e96a233d3b7703d7bbde96d173
MonicaCabanas23 <00079921@uca.edu.sv>
2022-11-27 20:27:16 -0600

    ✨ Add empty-box image for filtered products

--- Client/src/Components/FilteredProducts/Products.jsx
+++ Client/src/Components/FilteredProducts/Products.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const ProductsContainer = ({filteredUrl}) => {
     const [products, setProducts] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(true);
 
     useEffect(() => {
         const getData = async () => {
@@ -15,16 +16,32 @@ const ProductsContainer = ({filteredUrl}) => {
         getData();
     }, []);
 
+    useEffect(() => {
+        if (products.length > 0)
+            setIsEmpty(false);
+        else 
+            setIsEmpty(true);
+    }, [products])
+
     return (
         <section className="filtered-products">
             <h1 className="title">Resultados de la búsqueda</h1>
             <div className="cards">
-                {products.map((item, index) => {
-                    /* La ruta a la que redirigirá cada producto es ProductDescription */
-                    return (
-                        <Link to={{ pathname: '/product/', hash: item._id }} key={index}><ProductCard image={item.picture} name={item.name} price={item.price} /></Link>
-                    )
-                })}
+                {
+                    isEmpty ? 
+                    <div className='information-message'>
+                        <figure>
+                            <img src="https://res.cloudinary.com/cabrera-evil/image/upload/v1669599268/prettygirl-api/default/box_bjknfp.png" alt="empty" />
+                        </figure>
+                        <p> No se encontraron resultados :/ </p>
+                    </div> :
+                    products.map((item, index) => {
+                        /* La ruta a la que redirigirá cada producto es ProductDescription */
+                        return (
+                            <Link to={{ pathname: '/product/', hash: item._id }} key={index}><ProductCard image={item.picture} name={item.name} price={item.price} /></Link>
+                        )
+                    })
+                }
             </div>
         </section>
     )

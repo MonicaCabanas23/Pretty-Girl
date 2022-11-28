3c40511677531fae0e54d605575cb5164e98a715
cabrera-evil <douglascabrera.dev@gmail.com>
2022-11-27 19:33:03 -0600

    Fix: Footer filters

--- Client/src/Components/Footer/Products/Products.jsx
+++ Client/src/Components/Footer/Products/Products.jsx
@@ -1,39 +1,111 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import './Products.scss'
 
 const Products = () => {
-
+  const navigate = useNavigate();
   return (
     <div className="footer-products">
-        <h3>Productos</h3>
-        <Link to='/feed/filtered'>
-          <p>Accesorios</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Bikinis</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Bolsos</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Camisas</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Lentes</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Pantalones</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Shorts</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Vestidos</p>
-        </Link>
-        <Link to='/feed/filtered'>
-          <p>Zapatos</p>
-        </Link>
+      <h3>Productos</h3>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Accesorios"
+          }
+        })
+      }}>Accesorios</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Bikini"
+          }
+        })
+      }}>Bikinis</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Bolsos"
+          }
+        })
+      }}>Bolsos</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Camisas"
+          }
+        })
+      }}>Camisas</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Lentes"
+          }
+        })
+      }}>Lentes</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Pantalones"
+          }
+        })
+      }}>Pantalones</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Shorts"
+          }
+        })
+      }}>Shorts</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Vestidos"
+          }
+        })
+      }}>Vestidos</p>
+      <p onClick={(e) => {
+        // Reload page if we are in the same page
+        if (window.location.pathname === '/feed/filtered')
+          window.location.reload();
+        // Navigate to the filtered page
+        navigate('/feed/filtered', {
+          state: {
+            filteredUrl: "/api/products?category=Zapatos"
+          }
+        })
+      }}>Zapatos</p>
     </div>
   )
 }
